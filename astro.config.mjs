ca891bcd32ea65416c1271618d7d0402b878be57
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2024-03-10 20:40:36 +0100

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: 'ppacer docs',
+            social: {
+                github: 'https://github.com/ppacer',
+            },
+            sidebar: [
+                {
+                    label: 'Getting started',
+                    items: [
+                        { label: 'Intro', link: '/start/intro/' },
+                    ],
+                },
+                {
+                    label: 'Overview',
+                    items: [
+                        { label: 'High-level design', link: '' },
+                    ]
+                },
+                {
+                    label: 'Internals',
+                    items: [
+                        { label: 'DagWatcher', link: '' },
+                        { label: 'TaskScheduler', link: '' },
+                        { label: 'Schedule', link: '' },
+                        { label: 'Loggers', link: '' },
+                        { label: 'Databases', link: '' },
+                    ]
+                },
+            ],
+        }),
+    ],
+});
---
+++ src/content/docs/start/intro.md
@@ -0,0 +1,23 @@
+---
+title: Getting started with ppacer
+---
+
+# Installing ppacer
+
+
+```bash
+mkdir ppacer-demo && cd ppacer-demo
+go mod init ppacer_demo
+go get github.com/ppacer/core@latest
+go get github.com/ppacer/core/...
+```
+
+
+# Setting up scheduler and executor
+
+```go
+package main
+
+func main() {
+}
+```
