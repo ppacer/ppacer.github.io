1ac1ccd05df228fa5c2566655c267fff5d62657d
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2024-03-10 20:45:13 +0100

    Add GH workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://ppacer.github.io',
     integrations: [
         starlight({
             title: 'ppacer docs',
