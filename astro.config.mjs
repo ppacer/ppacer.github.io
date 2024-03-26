57a8515b79f3a27ccf9a8ec35bb580159d2f9b3e
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2024-03-26 18:45:22 +0100

    Change CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://docs.ppacer.org',
+    site: 'https://ppacer.org',
     integrations: [
         starlight({
             title: 'ppacer docs',
